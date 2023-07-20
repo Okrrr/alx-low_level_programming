0c9f48a8e98b3e5baf7dcec7bb6ac4ac8d56ae34
Okrrr <brian.korir00@gmail.com>
2023-07-20 13:39:20 -0700

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (i = 3; i < 782849; i = i + 2)
 	{
-	while ((n % i == 0) && (n != 1))
-				n = n / i;
-	}
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}	
 	printf("%lu\n", n);
 	return (0);
 }
