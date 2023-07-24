4e75d2b31ea385b3aaa03cc65d4848eb5b9fe25d
Okrrr <brian.korir00@gmail.com>
2023-07-24 08:07:23 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: used reference pointer
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int start = 0;
+
+	while (s[start])
+		start++;
+	while (start--)
+		_putchar(s[start]);
+	_putchar('\n');
+}
